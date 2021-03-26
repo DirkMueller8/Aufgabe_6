8de34369b3e59dbba33128f4ab2a715923f8df37
Dirk Mueller <dirkmuller8@outlook.de>
2021-03-26 06:29:49 +0100

    Final

--- Aufgabe_6/MainWindow.xaml.cs
+++ Aufgabe_6/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace Aufgabe_6
 {
@@ -22,20 +10,25 @@ namespace Aufgabe_6
     // 1. On button click the text in two labels is interchanged
     // 2. On placing a check in a checkbox the size in both labels is increased
     // 3. After unchecking the checkbox the font sizes shall revert to the original
+    // (the original font sized is queried on start of the program)
 
 
     public partial class MainWindow : Window
     {
+        int fontSizeLabelOnStart;
         string tempLabelText;
+        const int FONT_SIZE_INCREASE = 4;
         public MainWindow()
         {
             InitializeComponent();
-            int fontSizeLabel = (int)lbl_1.FontSize;
-            MessageBox.Show(fontSizeLabel.ToString());
+
+            // Store the font size of the label upon initialization of the WPF form:
+            fontSizeLabelOnStart = (int)lbl_1.FontSize;
         }
 
         private void btnExchange_Click(object sender, RoutedEventArgs e)
         {
+            // Code block to swap the content of the two labels:
             tempLabelText = lbl_1.Content.ToString();
             lbl_1.Content = lbl_2.Content;
             lbl_2.Content = tempLabelText;
@@ -43,8 +36,21 @@ namespace Aufgabe_6
 
         private void chbSize_Checked(object sender, RoutedEventArgs e)
         {
-            lbl_1.FontSize = 16;
-            lbl_2.FontSize = 16;
+            // Increase font size by constant:
+            lbl_1.FontSize += FONT_SIZE_INCREASE;
+            lbl_2.FontSize += FONT_SIZE_INCREASE;
+        }
+
+        private void chbSize_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Revert font size to the initial value in both labels:
+            lbl_1.FontSize = fontSizeLabelOnStart;
+            lbl_2.FontSize = fontSizeLabelOnStart;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
